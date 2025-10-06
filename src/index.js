45741a5cb58f8695c254812e2a556bdb53b0008c
Evan <evan1632885@gmail.com>
2025-10-06 11:54:48 +0800

    [main] Update index.js (OK operate version)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import cors from 'cors';
+
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(cors());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
